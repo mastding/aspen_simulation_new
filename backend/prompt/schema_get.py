f93366e4123377c2179b97be588114c4bba930c3
mastding <dinglei23@huawei.com>
2026-02-02 10:12:10 +0800

    author:dinglei data:20260202 content:第2次合并代码

--- backend/prompt/schema_get.py
+++ backend/prompt/schema_get.py
@@ -2,19 +2,27 @@ schema_get_prompt = f"""
 调用工具时基于用户描述的化工任务，思考本次任务需要用到如下哪些设备，将需要用到的设备类型作为请求参数传递给工具：
 当前支持的设备类型:
   "supported_block_types": [
-    "Mixer",
-    "RadFrac",
-    "Valve",
-    "Compr",
-    "Heater",
-    "Pump",
-    "RStoic",
-    "RPlug",
-    "Flash2",
-    "Decanter",
-    "Sep",
-    "Sep2",
-    "DSTWU"
-  ]
+	"Mixer",
+	"RadFrac",
+	"Valve",
+	"Compr",
+	"MCompr",
+	"Heater",
+	"Pump",
+	"RStoic",
+	"RPlug",
+	"RCSTR",
+	"Flash2",
+	"Flash3",
+	"Decanter",
+	"Sep2",
+	"Sep",
+	"Distl",
+	"Dupl",
+	"Extract",
+	"FSplit",
+	"DSTWU",
+	"HeatX"
+]
 """
 
